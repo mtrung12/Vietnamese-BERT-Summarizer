a26a9c5fda79ef07c696c504e2598d84c17688bf
Ngô Minh Trung <trung.nm226004@sis.hust.edu.vn>
2025-11-10 21:33:35 +0700

    add max length

--- embedding.py
+++ embedding.py
@@ -4,18 +4,21 @@ import torch
 from config import VNCORENLP_PATH
 from vnnlpcore import mvn_word_tokenize
 
-MAX_LENGTH = 256
 
 def get_sentence_embeddings(sentences, model_name, device):
     if "vibert4news" in model_name.lower():
         tokenizer = BertTokenizer.from_pretrained(model_name)
         model = AutoModel.from_pretrained(model_name)
+        MAX_LENGTH = 512
     elif "phobert" in model_name.lower():
+        MAX_LENGTH = 256
         tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=False)
         model = AutoModel.from_pretrained(model_name)
     else:
         tokenizer = AutoTokenizer.from_pretrained(model_name)
         model = AutoModel.from_pretrained(model_name)
+        MAX_LENGTH = 512
+
         
     model.to(device)
     model.eval()
@@ -35,7 +38,7 @@ def get_sentence_embeddings(sentences, model_name, device):
 
     for i in range(0, len(valid_sentences), batch_size):
         batch_sents = valid_sentences[i:i+batch_size]
-        inputs = tokenizer(batch_sents, padding=True, truncation=True, max_length=MAX_LENGTH, return_tensors="pt").to(device)
+        inputs = tokenizer(batch_sents, padding=True, truncation=False, return_tensors="pt").to(device)
         with torch.no_grad():
             outputs = model(**inputs)
             cls_embeddings = outputs.last_hidden_state[:, 0, :]
